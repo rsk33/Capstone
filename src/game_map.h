a50032df82511109f8649f8d72f9370e2ece3538
Roman Skybinskyi <r.skybinskyi@gmail.com>
2019-12-18 00:36:20 +0100

    style: remove  commented out code

--- src/game_map.h
+++ src/game_map.h
@@ -11,9 +11,8 @@ class GameMap
   public:
     GameMap(const std::string &filename, std::size_t grid_width, std::size_t grid_height);
 
-    bool ObstacleCell(int x, int y) { return _pixels[y][x]; }  // retun an element: false - free block, true - obstacle
-    // bool ObstacleCell(int x, int y);
-    std::vector<SDL_Point> obstacles;
+    bool ObstacleCell(int x, int y) { return _pixels[y][x]; }
+    std::vector<SDL_Point> obstacles; 
 
   private:
     // 2D array: false - free block, true - obstacle
